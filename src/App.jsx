958f49c6f50f02486d38b11c2d43e6fd594fdcb8
joe-newm <j.newm500@gmail.com>
2025-02-23 14:38:39 -0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
+import Nav from "./components/Nav.jsx"
+import About from "./pages/About.jsx"
 import './App.css'
 
 function App() {
@@ -8,9 +10,10 @@ function App() {
 
   return (
     <Router>
-
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
 
       </Routes>
 
