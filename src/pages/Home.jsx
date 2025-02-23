937d7ceb0d27c714c9b10aeacaacb7a4bdef799b
joe-newm <j.newm500@gmail.com>
2025-02-23 16:11:38 -0600

    add tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
-
+import headerImage from "../assets/gray.jpg";
 
 
 export default function Home() {
 
 
   return (
-    <h1> Hello World </h1>
+    <>      <img src={headerImage} class="headerImage" />
+      <div class="container mx-auto mt-10">
+
+        <h1 > Hello World </h1>
+      </div>
+    </>
   )
 }
