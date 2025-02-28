d115323badcb8351dd3b4d356ef18eb611c2347d
Joe-Newm <j.newm500@gmail.com>
2025-02-28 11:52:18 -0600

    add form to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   const navigate = useNavigate();
 
   const signout = async () => {
@@ -15,6 +18,10 @@ export default function Dashboard() {
     }
   }
 
+  const submitPost = async () => {
+    console.log('post')
+  }
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col container mx-auto mt-10 justify-center items-center gap-10">
@@ -22,8 +29,33 @@ export default function Dashboard() {
         <button onClick={signout}> sign out </button>
         <hr className="border-2 w-lvw mb-10" />
       </div>
-      <div>
-        <p>hello</p>
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center mt-10 w-100 mb-20">
+          <h2 className="text-2xl border-b-2 mb-5">New Art Post</h2>
+          <form onSubmit={submitPost} className="flex flex-col">
+            <label htmlFor="title">Art Title</label>
+            <input
+              id="title"
+              className="border-2 w-full h-10 pl-2 bg-white mb-5"
+              name="title"
+              type="text"
+              required
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <label htmlFor="desc">Art Description</label>
+            <input
+              id="desc"
+              className="border-2 w-full h-10 pl-2 bg-white h-20"
+              name="desc"
+              type="text"
+              required
+              placeholder="Description"
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </form>
+
+        </div>
       </div>
     </div>
   )
