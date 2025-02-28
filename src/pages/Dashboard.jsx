96091873afb96dc60e63b974d5873bcc3626ed10
joe-newm <j.newm500@gmail.com>
2025-02-27 21:11:58 -0600

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,18 +8,23 @@ export default function Dashboard() {
   const signout = async () => {
     try {
       signOut(auth);
-      console.log('you signed out successfully')
-      navigate('/login')
+      console.log('you signed out successfully');
+      navigate('/login');
     } catch (error) {
-      console.log('error signing out:', error)
+      console.log('error signing out:', error);
     }
   }
 
   return (
-    <div className="flex container mx-auto mt-10 justify-center items-center gap-10">
-      <p>Dashboard you are now logged in</p>
-      <button onClick={signout}> sign out </button>
-      <hr />
+    <div className="container mx-auto">
+      <div className="flex container mx-auto mt-10 justify-center items-center gap-10">
+        <p>Dashboard you are now logged in</p>
+        <button onClick={signout}> sign out </button>
+        <hr />
+      </div>
+      <div>
+        <p>hello</p>
+      </div>
     </div>
   )
 }
