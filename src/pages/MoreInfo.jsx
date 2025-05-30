b14aec4a9970d47f29939bec6c2ae669328ee349
Joe-Newm <j.newm500@gmail.com>
2025-05-30 10:48:08 -0500

    change text for availability more info page

--- src/pages/MoreInfo.jsx
+++ src/pages/MoreInfo.jsx
@@ -9,10 +9,10 @@ export default function MoreInfo() {
       <img src={post.imageURL} alt="painting" className="w-xl" />
       <div>
         <h1>{post.name}</h1>
-        <p>{post.description}</p>
+        <p className="mb-24">{post.description}</p>
         {post.availability === false ?
-          <h2>Art piece is currently unavailable.</h2>
-        : <h2>Contact for pricing</h2>}
+          <h2>This artwork has been sold.</h2>
+        : <h2>Still available — get in touch for details.</h2>}
       </div>
     </div>
   )
