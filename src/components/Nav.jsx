81fc1ae3b7eacd6b488ee17c918626433cc3fc7f
Joe-Newm <j.newm500@gmail.com>
2025-05-30 22:57:10 -0500

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,9 @@ export default function Nav() {
 
   return (
     <nav className="align-center items-center">
-      <div className="container mx-auto flex justify-between h-full">
-        <div className="pt-1">
-          <Link to="/" className="text-[45px] font-serif">Gray Risinger</Link>
+      <div className="container mx-auto flex justify-between h-full items-center">
+        <div className="">
+          <Link to="/" className="text-[35px] md:text-[45px] font-serif whitespace-nowrap">Gray Risinger</Link>
         </div>
         <div className="flex gap-6 items-center">
           <Link to="/" className="">Work</Link>
